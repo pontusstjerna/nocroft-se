f04c8b403fa4766212dee5df4190125a8804ed34
Pontus Stjernström <pontus@stjernstrom.nu>
2017-04-07 08:27:44 +0200

    Added buttons for start and stop. Gonna CSS those!

--- balls/balls.js
+++ balls/balls.js
@@ -1,47 +1,59 @@
-var world = document.getElementById("worldDiv");
-world.onclick = startAnimation;
-var animationStarted = false;
+window.onload = function(){
 
-var ball = new ball(document.getElementById("ball"), 1,2);
+    var world = document.getElementById("worldDiv");
+    var animationStarted = false;
+    var out = document.getElementById("info");
+    //document.getElementById("btnStop").onclick = stopAnimation;
 
-function ball(svgCircle, vx, vy){
-    this.x = parseFloat(svgCircle.getAttribute("cx"));
-    this.y = parseFloat(svgCircle.getAttribute("cy"));
-    this.vx = vx;
-    this.vy = vy;
-    this.radius = parseFloat(svgCircle.getAttribute("r"));
+    var ball = new ball(document.getElementById("ball"), Math.floor(Math.random()*6-3),Math.floor(Math.random()*6-3));
 
-    this.worldWidth = world.getAttribute("width");
-    this.worldHeight = world.getAttribute("height");
+    function ball(svgCircle, vx, vy){
+        this.x = parseFloat(svgCircle.getAttribute("cx"));
+        this.y = parseFloat(svgCircle.getAttribute("cy"));
+        this.vx = vx;
+        this.vy = vy;
+        this.radius = parseFloat(svgCircle.getAttribute("r"));
 
-    this.move = function(){
-        if(this.x + this.radius + this.vx > this.worldWidth || 
-            this.x - this.radius + this.vx < 0) 
-            this.vx *= -1;
-        
-        if(this.y + this.radius + this.vy > this.worldHeight || 
-            this.y - this.radius + this.vy < 0) 
-            this.vy *= -1;
+        this.worldWidth = world.getAttribute("width");
+        this.worldHeight = world.getAttribute("height");
 
-        this.x += this.vx;
-        this.y += this.vy;
+        this.move = function(){
+            if(this.x + this.radius + this.vx > this.worldWidth || 
+                this.x - this.radius + this.vx < 0) 
+                this.vx *= -1;
+            
+            if(this.y + this.radius + this.vy > this.worldHeight || 
+                this.y - this.radius + this.vy < 0) 
+                this.vy *= -1;
 
-        svgCircle.setAttribute("cx", this.x);
-        svgCircle.setAttribute("cy", this.y);
-    }
-};
+            this.x += this.vx;
+            this.y += this.vy;
 
-var timer; 
-function startAnimation(){
-    if(!animationStarted){
-        animationStarted = true;
-        timer = setInterval(moveBall, 10);
-    }else{
+            svgCircle.setAttribute("cx", this.x);
+            svgCircle.setAttribute("cy", this.y);
+
+            //document.getElementById("info").innerHTML = "vy: " + vy;
+        }
+    };
+
+    var timer; 
+    function startAnimation(){
+        if(!animationStarted){
+            animationStarted = true;
+            timer = setInterval(moveBall, 10);
+        }
+    };
+
+    function moveBall(){
+        ball.move();
+    };
+
+    function stopAnimation(){  
+        out.innerHTML = "pdkawwww";
         animationStarted = false;
         clearInterval(timer);
-    }
-};
+    };
 
-function moveBall(){
-    ball.move();
+
+    startAnimation();
 };
