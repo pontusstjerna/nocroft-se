1deb3325b29e1ddbb7221f79b2f4231d856f59f1
Pontus Stjernström <pontus@stjernstrom.nu>
2017-04-09 11:15:29 +0200

    Updated buttons. Tried to track mouse movements, got the position but cant create new ball.

--- balls/balls.js
+++ balls/balls.js
@@ -1,9 +1,12 @@
 window.onload = function(){
 
     var world = document.getElementById("worldDiv");
+    world.addEventListener("click", getMousePos, false);
     var animationStarted = false;
     var out = document.getElementById("info");
-    //document.getElementById("btnStop").onclick = stopAnimation;
+    var svgSurface = document.getElementById("svgWorld");
+    document.getElementById("btnStop").onclick = stopAnimation;
+    document.getElementById("btnStart").onclick = startAnimation;
 
     var ball = new ball(document.getElementById("ball"), Math.floor(Math.random()*6-3),Math.floor(Math.random()*6-3));
 
@@ -52,6 +55,20 @@ window.onload = function(){
         out.innerHTML = "pdkawwww";
         animationStarted = false;
         clearInterval(timer);
+    };
+
+    function getMousePos(e){
+        out.innerHTML = "Karin";
+        var x = e.layerX;
+        var y = e.layerY;
+
+        out.innerHTML = e.layerX;
+    };
+
+    function createNewBall(x, y){
+        //newBall = new SVGCircleElement();
+        
+        var newBall = svgSurface.createElement("circle");
     };
 
 
