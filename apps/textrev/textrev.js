44ac55a73b0e06142908a8bd5db7c9ef1ba61133
Pontus Stjernström <pontus@stjernstrom.nu>
2017-04-29 11:56:20 +0200

    Added a super simple text reverser similar to the generator just for funz. Also added credits for generator-algorithm and changed the footer of some pages.

---
+++ apps/textrev/textrev.js
@@ -0,0 +1,26 @@
+document.getElementById("btnReverse").onclick = startReversion;
+var input = document.getElementById("txtIn");
+var output = document.getElementById("txtOut");
+
+function startReversion(){
+  reverse(input.value);
+}
+
+function reverse(text){
+    output.value = "";
+    var i = text.length;
+
+    var loop = setInterval(function(){
+      i--;
+
+      output.value += text.charAt(i);
+
+      if(i <= 0){
+        clearInterval(loop);
+      }
+    }, 0);    
+}
+
+function createExample(){
+  input.value = example;
+}
