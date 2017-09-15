0938d471617afbe5022a25f4bc3bd19ee219d085
Pontus Stjernström <pontus@stjernstrom.nu>
2017-09-15 15:54:50 +0200

    Began with adding first version of zapping problem.

--- apps/algorithms/minZapProblem.js
+++ apps/algorithms/minZapProblem.js
@@ -18,3 +18,38 @@ for i = 0 to sizeof(C) - 1 loop
     end if
 end loop
 */
+
+var surface = document.getElementById("balloonSurface");
+var div = document.getElementById("balloonDiv");
+var numBalloons = 1;
+
+div.onclick = (e) => {
+   getMousePos(e);
+}
+
+function getMousePos(e){
+    var x = e.pageX;
+    var y = e.pageY;
+    var svgW = surface.getAttribute("width");
+
+    if(document.body.clientWidth > svgW){
+        x -= (document.body.clientWidth - svgW)/2;
+    }
+
+    addBalloon(x,y);
+};
+
+function addBalloon(x,y) {
+    var newBalloon = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+
+    newBalloon.setAttribute("cx",x);
+    newBalloon.setAttribute("cy",y);
+    newBalloon.setAttribute("id", "balloon" + numBalloons);
+    newBalloon.setAttribute("r", Math.random()*25 + 5);
+    newBalloon.style.stroke = "green";
+    newBalloon.style.strokeWidth = 2;
+    newBalloon.style.fill = "white";
+
+    surface.appendChild(newBalloon);
+    numBalloons++;
+}
