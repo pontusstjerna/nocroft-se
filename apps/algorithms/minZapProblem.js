5379cb193bd6a6fddd8094928183ee79c0255ee6
Pontus Stjernström <pontus@stjernstrom.nu>
2017-09-16 16:11:39 +0200

    Finally first versin of algorithm works. Not optimal as expected.

--- apps/algorithms/minZapProblem.js
+++ apps/algorithms/minZapProblem.js
@@ -5,7 +5,6 @@ var rays = [];
 var origin = {x: surface.getAttribute("width")/2, y: surface.getAttribute("height")/2};
 
 addBalloon(100,50);
-addRay(100,0);
 
 div.onclick = (e) => {
    getMousePos(e);
@@ -37,7 +36,7 @@ function addBalloon(x,y) {
     if(dist(origin, {x: x, y: y}) <= radius)
         return;
 
-    for(i = 0; i < balloons.length; i++) {
+    for(var i = 0; i < balloons.length; i++) {
         if(intersectBalloons(balloon, balloons[i]))
             return;
     }
@@ -65,7 +64,7 @@ function addRay(x,y) {
     newRay.style.strokeWidth = 2;
 
     surface.appendChild(newRay);
-    rays.push({x: x, y: y});
+    rays.push({x: x, y: y, svg: newRay});
 }
 
 function dist(p1, p2) {
@@ -73,11 +72,42 @@ function dist(p1, p2) {
 }
 
 function intersect(r, c) {
-    return dist(r, c) < c.radius;
+    //p1 is the first line point
+    //p2 is the second line point
+    //c is the circle's center
+    //r is the circle's radius
+    var p1 = origin;
+    var p2 = r;
+
+    var p3 = {x:p1.x - c.x, y:p1.y - c.y}; //shifted line points
+    var p4 = {x:p2.x - c.x, y:p2.y - c.y};
+
+    var m = (p4.y - p3.y) / (p4.x - p3.x); //slope of the line
+    var b = p3.y - m * p3.x; //y-intercept of line
+
+    var underRadical = Math.pow(c.radius,2)*Math.pow(m,2) + Math.pow(c.radius,2) - Math.pow(b,2); //the value under the square root sign 
+
+    if (underRadical < 0) {
+        //line completely missed
+        return false;
+    } else {
+        var t1 = (-m*b + Math.sqrt(underRadical))/(Math.pow(m,2) + 1); //one of the intercept x's
+        var t2 = (-m*b - Math.sqrt(underRadical))/(Math.pow(m,2) + 1); //other intercept's x
+        var i1 = {x:t1+c.x, y:m*t1+b+c.y}; //intercept point 1
+        var i2 = {x:t2+c.x, y:m*t2+b+c.y}; //intercept point 2
+        return [i1, i2];
+    }
 }
 
 function intersectBalloons(b1, b2) {
     return dist(b1, b2) < b1.radius + b2.radius;
+}
+
+function clearRays() {
+    for(var i = 0; i < rays.length; i++) {
+        surface.removeChild(rays[i].svg);
+    }
+    rays = [];
 }
 
 /*****************THE ALGORITHM******************/
@@ -90,12 +120,11 @@ let S = {}
 
 for i = 0 to sizeof(C) - 1 loop
     if not H[i] then
-        H[i] = true
         let r = ray from origin to origin of C[i]
-        for j = i to sizeof(C) - 1
+        add r to S
+        for j = i to sizeof(C) - 1 loop
             if intersect(r, C[j]) then
                 H[j] = true
-                add r to S
             end if
         end loop
     end if
@@ -103,24 +132,47 @@ end loop
 */
 
 function try1() {
+    clearRays();
     let C = sortBalloons();
-    console.log(C.length);
+    let H = createArrayWithValue(C.length, false);
+
+    for(var i = 0; i < C.length; i++) {
+        if(!H[i]) {
+            let r = {x: C[i].x, y: C[i].y};
+            addRay(r.x, r.y);
+            for(var j = i; j < C.length; j++) {
+                if(intersect(r, C[j])) {
+                    H[j] = true;
+                }
+            }
+        }
+    }
     
 }
 
 function sortBalloons() {
     let sorted = [];
     let copy = balloons.slice();
-    for(j = 0; j < balloons.length; j++) {
+    for(var j = 0; j < balloons.length; j++) {
         let current = copy[0];
-        for(i = 0; i < copy.length; i++) {
+        for(var i = 0; i < copy.length; i++) {
             if(dist(current, origin) < dist(copy[i], origin)) {
                 current = copy[i];
             } 
         }
         sorted.push(current);
-        copy = copy.splice(copy.indexOf(current) - 1,1);
+        let index = copy.indexOf(current);
+        copy.splice(index ,1);
     }
 
     return sorted;
+}
+
+function createArrayWithValue(length, value) {
+    let arr = [];
+    for(var i = 0; i < length; i++) {
+        arr[i] = value;
+    }
+
+    return arr;
 }
