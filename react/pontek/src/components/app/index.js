57d52c52766559e8e8360bcf2fe38e0b2da6442b
Pontus Stjernström <pontus@stjernstrom.nu>
2018-01-20 17:57:11 +0100

    Added bootstrap and first links.

--- react/pontek/src/components/app/index.js
+++ react/pontek/src/components/app/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Symmetry from './symmetry';
+import windows from '../../assets/windows.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/default.css';
 
 class App extends Component {
@@ -8,13 +10,38 @@ class App extends Component {
         
         return (
             <div>
-                <header className="App-header">
+                <div className="center">
                     <Symmetry ref={svg => svg.animate()}/>
                     <h1>Under construction!</h1>
-                </header>
-                <p className="Text">
-                    This will later be the new version of Pontek.se.
-                </p>
+                    <p className="Text">
+                        This will later be the new version of Pontek.se.
+                    </p>
+                </div>
+                <div className="row">
+                    <div className="offset-md-5 col-md-2 text-center">
+                        <a href="downloads/Symmetry Screensaver.scr">
+                            <img src={windows} />
+                        </a>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="offset-md-3 col-md-6 text-center">
+                        <a href="downloads/Symmetry Screensaver.scr">
+                            {'Download '} 
+                        </a>
+                        this screensaver free for Windows or 
+                        <a href="https://play.google.com/store/apps/details?id=pontus.symmetry&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+                        {' download '}</a> 
+                        this live wallpaper for Android!
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="offset-md-1 col-md-2 text-center">
+                        <a href="">
+                            <img src="https://play.google.com/intl/en_us/badges/images/generic/en-play-badge.png" />
+                        </a>
+                    </div>
+                </div>
             </div>
         );
     }
