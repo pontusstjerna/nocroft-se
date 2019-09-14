ee376d335a13eaa2c67bb7ce9a14d2b92ad26e72
pontusstjerna <pontusstjernstrom@gmail.com>
2019-09-14 15:30:47 +0200

    changed port

--- src/components/pages/admin.js
+++ src/components/pages/admin.js
@@ -33,7 +33,7 @@ class Admin extends Component {
             <div className="p-surveillance">
                 {robotPi &&
                     <div className="container">
-                        <RobotPi socketURL="" token={token} videoURL={'wss://' + window.location.host.split(':')[0] + ':8080/video'} />
+                        <RobotPi socketURL="" token={token} videoURL={'wss://' + window.location.host.split(':')[0] + '/video'} />
                         <button onClick={() => this.setState({robotPi: false})}>Exit CatHunter </button>                        
                     </div>
                 }
