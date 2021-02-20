d2d11db91692bfd1d9bad1d9bd482e9b664572a9
Pontus Stjernström <pontus@stjernstrom.nu>
2021-02-20 10:19:34 +0100

    Update socket client and change action types to underscore

--- src/components/pages/catHunter/index.js
+++ src/components/pages/catHunter/index.js
@@ -1,338 +1,375 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import { getToken } from "../../../util/auth";
-import { connectIO } from './socket.js';
-import { API_URL } from '../login.js';
-import CtrlButton from './ctrlButton';
-import Controller from './controller';
-import * as types from './robotPiActionTypes';
+import { getToken } from '../../../util/auth'
+import { connectIO } from './socket.js'
+import CtrlButton from './ctrlButton'
+import Controller from './controller'
+import * as types from './robotPiActionTypes'
 
-import './style.css';
-import VideoStream from "../../molecules/videoStream";
-
-const socketURL = '';
+import './style.css'
+import VideoStream from '../../molecules/videoStream'
 
 class RobotPi extends Component {
+  constructor(props) {
+    super(props)
 
-    constructor(props) {
-        super(props);
-
-        // <RobotPi socketURL="" token={token} videoURL={'wss://' + window.location.host.split(':')[0] + '/video'} />
-
-        this.state = {
-            socket: undefined,
-            started: null,
-            lastConnected: null,
-            connecting: false,
-            error: '',
-            controller: null,
-            inputs: {
-                up: false,
-                left: false,
-                down: false,
-                right: false,
-            },
-            status: null,
-            chargingLoading: false
-        };
-
-        this.setupStatusInterval = this.setupStatusInterval.bind(this);
-        this.onKeyDown = this.onKeyDown.bind(this);
-        this.onKeyUp = this.onKeyUp.bind(this);
-        this.onError = this.onError.bind(this);
-        this.renderChargeButton = this.renderChargeButton.bind(this)
+    this.state = {
+      socket: undefined,
+      started: null,
+      lastConnected: null,
+      connecting: false,
+      error: '',
+      controller: null,
+      inputs: {
+        up: false,
+        left: false,
+        down: false,
+        right: false,
+      },
+      status: null,
+      chargingLoading: false,
     }
 
-    componentDidMount() {
-        const token = getToken();
+    this.setupStatusInterval = this.setupStatusInterval.bind(this)
+    this.onKeyDown = this.onKeyDown.bind(this)
+    this.onKeyUp = this.onKeyUp.bind(this)
+    this.onError = this.onError.bind(this)
+    this.renderChargeButton = this.renderChargeButton.bind(this)
+  }
 
-        connectIO(socketURL, token, this.onError)
-        .then(({socket, started}) => {
-            this.setState({
-                started: started.started,
-                lastConnected: started.lastConnected,
-                connecting: false,
-                controller: new Controller(socket),
-                socket
-            });
+  componentDidMount() {
+    const token = getToken()
 
-            this.setupStatusInterval(socket);
+    connectIO(token, this.onError)
+      .then(({ socket, started }) => {
+        this.setState({
+          started: started.started,
+          lastConnected: started.lastConnected,
+          connecting: false,
+          controller: new Controller(socket),
+          socket,
         })
-        .catch(this.onError);
 
-        this.setState({connecting: true,});
+        this.setupStatusInterval(socket)
+      })
+      .catch(this.onError)
 
-        document.addEventListener("keydown", this.onKeyDown);
-        document.addEventListener("keyup", this.onKeyUp);
+    this.setState({ connecting: true })
+
+    document.addEventListener('keydown', this.onKeyDown)
+    document.addEventListener('keyup', this.onKeyUp)
+  }
+
+  componentWillUnmount() {
+    const { socket, videoPlayer } = this.state
+
+    document.removeEventListener('keydown', this.onKeyDown)
+    document.removeEventListener('keyup', this.onKeyUp)
+
+    if (socket) socket.close()
+    if (videoPlayer) videoPlayer.stop()
+  }
+
+  setupStatusInterval(socket) {
+    socket.on('status', status => {
+      this.setState({
+        status: status && JSON.parse(status),
+        chargingLoading: false,
+      })
+      setTimeout(() => socket.emit('status'), 5000)
+    })
+    socket.emit('status')
+  }
+
+  onKeyDown(event) {
+    const { controller, inputs } = this.state
+    const { up, left, down, right } = inputs
+
+    switch (event.key) {
+      case 'ArrowLeft':
+        if (!left) {
+          if (up) {
+            controller.left()
+          } else if (down) {
+            controller.left()
+          } else {
+            controller.rotLeft()
+          }
+
+          this.setState({
+            inputs: {
+              ...this.state.inputs,
+              left: true,
+            },
+          })
+        }
+        break
+
+      case 'ArrowUp':
+        if (!up) {
+          if (left) {
+            controller.left()
+          } else if (right) {
+            controller.right()
+          } else {
+            controller.forward()
+          }
+
+          this.setState({
+            inputs: {
+              ...this.state.inputs,
+              up: true,
+            },
+          })
+        }
+        break
+
+      case 'ArrowRight':
+        if (!right) {
+          if (up) {
+            controller.right()
+          } else if (down) {
+            controller.right()
+          } else {
+            controller.rotRight()
+          }
+
+          this.setState({
+            inputs: {
+              ...this.state.inputs,
+              right: true,
+            },
+          })
+        }
+        break
+
+      case 'ArrowDown':
+        if (!down) {
+          controller.reverse()
+          if (left) {
+            controller.left()
+          } else if (right) {
+            controller.right()
+          } else {
+            controller.forward()
+          }
+
+          this.setState({
+            inputs: {
+              ...this.state.inputs,
+              down: true,
+            },
+          })
+        }
+        break
+
+      default:
+        return
     }
 
-    componentWillUnmount() {
-        const { socket, videoPlayer } = this.state;
+    event.preventDefault() // prevent the default action (scroll / move caret)
+  }
 
-        document.removeEventListener("keydown", this.onKeyDown);
-        document.removeEventListener("keyup", this.onKeyUp);
+  onKeyUp(event) {
+    const { controller, inputs } = this.state
+    const { up, left, down, right } = inputs
 
-        if (socket) socket.close();
-        if (videoPlayer) videoPlayer.stop();
-    }
-
-    setupStatusInterval(socket) {
-        socket.on('status', status => {
-           this.setState({status: JSON.parse(status), chargingLoading: false});
-            setTimeout(() => socket.emit('status'), 5000);
-        });
-        socket.emit('status');
-    }
-
-    onKeyDown(event) {
-        const { controller, inputs } = this.state;
-        const { up, left, down, right } = inputs;
-
-        switch(event.key) {
-            case 'ArrowLeft':
-                if (!left) {
-                    if (up) {
-                        controller.left();
-                    } else if (down) {
-                        controller.left();
-                    } else {
-                        controller.rotLeft();
-                    }
-
-                    this.setState({
-                        inputs: {
-                            ...this.state.inputs,
-                            left: true,
-                        }
-                    });
-                }
-                break;
-
-            case 'ArrowUp':
-                if (!up) {
-                    if (left) {
-                        controller.left();
-                    } else if (right) {
-                        controller.right();
-                    } else {
-                        controller.forward();
-                    }
-
-                    this.setState({
-                        inputs: {
-                            ...this.state.inputs,
-                            up: true,
-                        }
-                    });
-                }
-                break;
-
-            case 'ArrowRight':
-                if (!right) {
-                    if (up) {
-                        controller.right();
-                    } else if (down) {
-                        controller.right();
-                    } else {
-                        controller.rotRight();
-                    }
-
-                    this.setState({
-                        inputs: {
-                            ...this.state.inputs,
-                            right: true,
-                        }
-                    });
-                }
-                break;
-
-            case 'ArrowDown':
-                if (!down) {
-                    controller.reverse();
-                    if (left) {
-                        controller.left();
-                    } else if (right) {
-                        controller.right();
-                    } else {
-                        controller.forward();
-                    }
-
-                    this.setState({
-                        inputs: {
-                            ...this.state.inputs,
-                            down: true,
-                        }
-                    });
-                }
-                break;
-
-            default: return;
+    switch (event.key) {
+      case 'ArrowLeft':
+        if (up || down) {
+          controller.forward()
+        } else {
+          controller.stop()
         }
 
-        event.preventDefault(); // prevent the default action (scroll / move caret)
-    }
+        this.setState({
+          inputs: {
+            ...this.state.inputs,
+            left: false,
+          },
+        })
+        break
 
-    onKeyUp(event) {
-        const { controller, inputs } = this.state;
-        const { up, left, down, right } = inputs;
-
-        switch(event.key) {
-            case 'ArrowLeft':
-                if (up || down) {
-                    controller.forward();
-                } else {
-                    controller.stop();
-                }
-
-                this.setState({
-                    inputs: {
-                        ...this.state.inputs,
-                        left: false,
-                    }
-                });
-                break;
-
-            case 'ArrowUp':
-                if (left) {
-                    controller.rotLeft();
-                } else if (right) {
-                    controller.rotRight();
-                } else {
-                    controller.stop();
-                }
-
-                this.setState({
-                    inputs: {
-                        ...this.state.inputs,
-                        up: false,
-                    }
-                });
-                break;
-
-            case 'ArrowRight':
-                if (up || down) {
-                    controller.forward();
-                } else {
-                    controller.stop();
-                }
-
-                this.setState({
-                    inputs: {
-                        ...this.state.inputs,
-                        right: false,
-                    }
-                });
-                break;
-
-            case 'ArrowDown':
-                controller.reverse();
-                if (left) {
-                    controller.rotLeft();
-                } else if (right) {
-                    controller.rotRight();
-                } else {
-                    controller.stop();
-                }
-
-                this.setState({
-                    inputs: {
-                        ...this.state.inputs,
-                        down: false,
-                    }
-                });
-                break;
-
-            default: return;
+      case 'ArrowUp':
+        if (left) {
+          controller.rotLeft()
+        } else if (right) {
+          controller.rotRight()
+        } else {
+          controller.stop()
         }
 
-        event.preventDefault(); // prevent the default action (scroll / move caret)
-    }
+        this.setState({
+          inputs: {
+            ...this.state.inputs,
+            up: false,
+          },
+        })
+        break
 
-    onError(error) {
-        const { socket } = this.state;
-        if (socket) {
-            socket.close();
+      case 'ArrowRight':
+        if (up || down) {
+          controller.forward()
+        } else {
+          controller.stop()
         }
 
-        this.setState({error: String(error)});
-    }
+        this.setState({
+          inputs: {
+            ...this.state.inputs,
+            right: false,
+          },
+        })
+        break
 
-    render() {
-        const { started, lastConnected, connecting, error, controller, inputs, status, } = this.state;
-        const { up, left, down, right } = inputs;
-
-        return (
-            <div className="p-cathunter">
-                <h1>CatHunter 1.1</h1>
-                <VideoStream id="CatHunter" url="/cathunter" />
-                <div className="buttons">
-                    <CtrlButton action={types.LEFT} controller={controller} />
-                    <CtrlButton action={types.FORWARD} active={up} controller={controller} />
-                    <CtrlButton action={types.RIGHT} controller={controller} />
-                    <br/>
-                    <CtrlButton action={types.ROTATE_LEFT} active={left} controller={controller} />
-                    <CtrlButton action={types.REVERSE} active={down} controller={controller} />
-                    <CtrlButton action={types.ROTATE_RIGHT} active={right} controller={controller} />
-                </div>
-                { status && this.renderChargeButton() }
-                { error && 
-                    <p className="disconnected">{error}</p>
-                }
-                { connecting &&
-                    <p>Connecting...</p>
-                }
-                { started &&
-                    <p>Server last started {started}.</p>
-                }
-                { lastConnected &&
-                    <p>Last user disconnected {lastConnected}.</p>
-                }
-                { this.renderStatus(status) }
-            </div>
-        );
-    }
-
-    renderChargeButton() {
-        const { status: { isCharging }, chargingLoading, controller, socket } = this.state
-
-        if (isCharging === undefined || chargingLoading) {
-            return <p>Loading...</p>
+      case 'ArrowDown':
+        controller.reverse()
+        if (left) {
+          controller.rotLeft()
+        } else if (right) {
+          controller.rotRight()
+        } else {
+          controller.stop()
         }
 
-        const onClick = () => {
-            this.setState({ chargingLoading: true })
-            if (isCharging) {
-                controller.stopCharging()
-            } else {
-                controller.startCharging()
-            }
-            socket.emit('status')
-        }
+        this.setState({
+          inputs: {
+            ...this.state.inputs,
+            down: false,
+          },
+        })
+        break
 
-        return <button onClick={onClick}>{ isCharging ? 'Stop ' : 'Start ' }charging</button>
+      default:
+        return
     }
 
-    renderStatus(status) {
-        if (!status) {
-            return <p>Unable to get CatHunter system status.</p>;
-        }
+    event.preventDefault() // prevent the default action (scroll / move caret)
+  }
 
-        return (
-            <p>
-                { status.throttled }<br />
-                <b>Charging: </b>{ status.isCharging ? 'Yes' : 'No' }<br/>
-                <b>Temperature: </b>{ status.temp }<br />
-                { status.volts &&
-                    <span>
-                        <b>Core volts: </b>{ status.volts.core }<br />
-                        <b>SD RAM C volts: </b>{ status.volts.sdram_c }<br />
-                        <b>SD RAM I volts: </b>{ status.volts.sdram_i }<br />
-                        <b>SD RAM P volts: </b>{ status.volts.sdram_p }<br />
-                    </span>
-                }
-            </p>
-        );
+  onError(error) {
+    const { socket } = this.state
+    if (socket) {
+      socket.close()
     }
+
+    this.setState({ error: String(error) })
+  }
+
+  render() {
+    const {
+      started,
+      lastConnected,
+      connecting,
+      error,
+      controller,
+      inputs,
+      status,
+    } = this.state
+    const { up, left, down, right } = inputs
+
+    return (
+      <div className="p-cathunter">
+        <h1>CatHunter 1.1</h1>
+        <VideoStream id="CatHunter" url="/cathunter" />
+        <div className="buttons">
+          <CtrlButton action={types.LEFT} controller={controller} />
+          <CtrlButton
+            action={types.FORWARD}
+            active={up}
+            controller={controller}
+          />
+          <CtrlButton action={types.RIGHT} controller={controller} />
+          <br />
+          <CtrlButton
+            action={types.ROTATE_LEFT}
+            active={left}
+            controller={controller}
+          />
+          <CtrlButton
+            action={types.REVERSE}
+            active={down}
+            controller={controller}
+          />
+          <CtrlButton
+            action={types.ROTATE_RIGHT}
+            active={right}
+            controller={controller}
+          />
+        </div>
+        {status && this.renderChargeButton()}
+        {error && <p className="disconnected">{error}</p>}
+        {connecting && <p>Connecting...</p>}
+        {started && <p>CatHunter last started {started}.</p>}
+        {lastConnected && <p>Last user disconnected {lastConnected}.</p>}
+        {this.renderStatus(status)}
+      </div>
+    )
+  }
+
+  renderChargeButton() {
+    const {
+      status: { isCharging },
+      chargingLoading,
+      controller,
+      socket,
+    } = this.state
+
+    if (isCharging === undefined || chargingLoading) {
+      return <p>Loading...</p>
+    }
+
+    const onClick = () => {
+      this.setState({ chargingLoading: true })
+      if (isCharging) {
+        controller.stopCharging()
+      } else {
+        controller.startCharging()
+      }
+      socket.emit('status')
+    }
+
+    return (
+      <button onClick={onClick}>
+        {isCharging ? 'Stop ' : 'Start '}charging
+      </button>
+    )
+  }
+
+  renderStatus(status) {
+    if (!status) {
+      return <p>Unable to get CatHunter system status.</p>
+    }
+
+    return (
+      <p>
+        {status.throttled}
+        <br />
+        <b>Charging: </b>
+        {status.isCharging ? 'Yes' : 'No'}
+        <br />
+        <b>Temperature: </b>
+        {status.temp}
+        <br />
+        {status.volts && (
+          <span>
+            <b>Core volts: </b>
+            {status.volts.core}
+            <br />
+            <b>SD RAM C volts: </b>
+            {status.volts.sdram_c}
+            <br />
+            <b>SD RAM I volts: </b>
+            {status.volts.sdram_i}
+            <br />
+            <b>SD RAM P volts: </b>
+            {status.volts.sdram_p}
+            <br />
+          </span>
+        )}
+      </p>
+    )
+  }
 }
 
-export default RobotPi;
+export default RobotPi
--- src/components/pages/catHunter/socket.js
+++ src/components/pages/catHunter/socket.js
@@ -1,36 +1,35 @@
-import io from 'socket.io-client';
-// import jsmpeg from 'jsmpeg';
+import io from 'socket.io-client'
 
-export const connectIO = (url, token, onError) => {
-    return new Promise((resolve, reject) => {
-        let socket = io(url, token ? {
-            transportOptions: {
-                polling: {
-                    extraHeaders: {
-                        'Authorization': 'bearer ' + token,
-                    }
-                }
-            }
-        } : {});
+export const connectIO = (token, onError) => {
+  return new Promise((resolve, reject) => {
+    const socket = io(token ? { auth: { token: token } } : {})
 
-        const timeout = setTimeout(() => {
-            socket.disconnect();
-            reject('Timeout: Unable to connect to the CatHunter server.');
-        }, 7000);
+    const timeout = setTimeout(() => {
+      socket.disconnect()
+      reject('Timeout: Unable to connect to the CatHunter server.')
+    }, 7000)
 
-        socket.on('unauthorized', () => onError('Unauthorized.'));
-        socket.on('error', onError);
-        socket.on('disconnect', () => onError('Unable to connect to CatHunter server.'));
-        socket.on('connect_timeout', () => onError('Connection to server timed out.'));
-        socket.on('reconnect_timeout', () => onError('Connection to server timed out.'));
+    socket.on('unauthorized', () => onError('Unauthorized.'))
+    socket.on('error', onError)
+    socket.on('disconnect', () =>
+      onError('Unable to connect to CatHunter server.')
+    )
+    socket.on('connect_timeout', () =>
+      onError('Connection to server timed out.')
+    )
+    socket.on('reconnect_timeout', () =>
+      onError('Connection to server timed out.')
+    )
 
-        socket.on('started', started => resolve({socket, started: JSON.parse(started)}));
+    socket.on('started', started =>
+      resolve({ socket, started: JSON.parse(started) })
+    )
 
-        socket.on('connect', () => {
-            setTimeout(() => {
-                socket.emit('started');
-                clearTimeout(timeout)
-            }, 500);
-        });
-    });
-};
+    socket.on('connect', () => {
+      setTimeout(() => {
+        socket.emit('started')
+        clearTimeout(timeout)
+      }, 500)
+    })
+  })
+}
