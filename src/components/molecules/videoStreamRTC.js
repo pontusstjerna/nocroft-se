c3cb08f8357ff687d302ebdbd83e5daaaac5a61a
Pontus Drejer <pontus@pythomspace.se>
2025-01-02 22:01:11 +0100

    really try to get webrtc working with turn server

--- src/components/molecules/videoStreamRTC.js
+++ src/components/molecules/videoStreamRTC.js
@@ -13,51 +13,93 @@ export default function VideoStreamRTC({ source, token, width, height }) {
         if (!token) {
             return
         }
-
-        setLoading(true)
-        const peerConnection = new RTCPeerConnection({
-            sdpSemantics: 'unified-plan'
+        const socket = io(`/video`, {
+            auth: { token, room: source },
         })
 
-        peerConnection.ontrack = event => {
-            console.log(`Got ${event.track.kind} event from peer connection.`)
-            if (event.track.kind === "video") {
-                videoRef.current.srcObject = event.streams[0]
-                setLoading(false)
+        socket.on('connect', () => {
+            console.log("Socket connected to " + source)
+            setLoading(true)
+            const peerConnection = new RTCPeerConnection({
+                iceServers: [
+                    {
+                        urls: "stun:stun.l.google.com:19302"
+                    },
+                    {
+                        urls: "turn:relay1.expressturn.com:3478",
+                        username: "efLC34KD51PQ4T537Q",
+                        credential: "83MYDN4EZQTHNHhS"
+                    },
+                    {
+                        urls: "turn:nocroft.se:3478",
+                        username: "webserver",
+                        credential: "maskros"
+                    },
+                    {
+                        urls: "stun:stun.relay.metered.ca:80",
+                    },
+                    {
+                        urls: "turn:global.relay.metered.ca:80",
+                        username: "c7f548a90dd81acc1222ae5c",
+                        credential: "1RaGdDCrbvyGc/To",
+                    },
+                    {
+                        urls: "turn:global.relay.metered.ca:80?transport=tcp",
+                        username: "c7f548a90dd81acc1222ae5c",
+                        credential: "1RaGdDCrbvyGc/To",
+                    },
+                    {
+                        urls: "turn:global.relay.metered.ca:443",
+                        username: "c7f548a90dd81acc1222ae5c",
+                        credential: "1RaGdDCrbvyGc/To",
+                    },
+                    {
+                        urls: "turns:global.relay.metered.ca:443?transport=tcp",
+                        username: "c7f548a90dd81acc1222ae5c",
+                        credential: "1RaGdDCrbvyGc/To",
+                    },
+                ],
+            })
+
+            peerConnection.ontrack = event => {
+                console.log(`Got ${event.track.kind} event from peer connection.`)
+                if (event.track.kind === "video") {
+                    videoRef.current.srcObject = event.streams[0]
+                    setLoading(false)
+                }
             }
-        }
-        peerConnection.addTransceiver("video", { direction: "recvonly" })
+            peerConnection.addTransceiver("video", { direction: "recvonly" })
 
-        peerConnection.createOffer().then(offer =>
-            peerConnection.setLocalDescription(offer)
-        )
-            .then(() => new Promise(resolve => peerConnection.onicecandidate = () => resolve()))
-            .then(() => {
-                const offer = peerConnection.localDescription
-                return fetch(`${API_URL}/video/${source}/offer`, {
-                    method: "POST",
-                    body: JSON.stringify({
-                        sdp: offer.sdp, type: offer.type
-                    }),
-                    headers: {
-                        "Authorization": `bearer ${token}`,
-                        'Content-Type': 'application/json'
+            peerConnection.createOffer().then(offer =>
+                peerConnection.setLocalDescription(offer)
+            )
+                .then(() => new Promise(resolve => peerConnection.onicecandidate = () => resolve()))
+                .then(() => {
+                    const offer = peerConnection.localDescription
+                    return fetch(`${API_URL}/video_offer/${source}`, {
+                        method: "POST",
+                        body: JSON.stringify({
+                            offer: { sdp: offer.sdp, type: offer.type },
+                            socketId: socket.id
+                        }),
+                        headers: {
+                            "Authorization": `bearer ${token}`,
+                            'Content-Type': 'application/json'
 
-                    }
+                        }
+                    })
+                }).then(() => {
+                    socket.removeListener("answer")
+                    socket.on("answer", answer => {
+                        console.log("Got video answer, starting stream!")
+                        if (peerConnection) {
+                            peerConnection.setRemoteDescription(answer)
+                        }
+                    })
                 })
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Not ok!")
-                }
-                return response.json()
-            })
-            .then(answer => {
-                console.log("Got video answer, starting stream!")
-                if (peerConnection) {
-                    peerConnection.setRemoteDescription(answer)
-                }
-            })
+        })
+
+
     }, [token])
 
     return <Container>
